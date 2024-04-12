3565258653d8ec9ffe7fcd1a0b74c25f1755c3e8
arnab125 <arnabtasnimpranto@gmail.com>
2024-04-12 07:29:40 +0600

    restarted learning

--- lib/main.dart
+++ lib/main.dart
@@ -3,138 +3,135 @@ import 'dart:math';
 
 void main() {
   runApp(
-    const MyApp(),
+    MyApp(),
   );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Board();
+    return const MaterialApp(
+      home: HomePage(),
+    );
   }
 }
 
-class Board extends StatefulWidget {
-  const Board({Key? key}) : super(key: key);
-
-  @override
-  _BoardState createState() => _BoardState();
-}
-
-class _BoardState extends State<Board> {
-  var leftDiceIndex = 0;
-  var rightDiceIndex = 0;
-  var sum = 0;
-  String verdict = "";
-  bool isRolling = false;
-  bool published = false;
-  final Random random = Random.secure();
+class HomePage extends StatelessWidget {
+  const HomePage({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
+    return Scaffold(
         appBar: AppBar(
-          title: const Text("My First App", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+          title:
+              Text('App Name', style: TextStyle(fontWeight: FontWeight.bold)),
+          backgroundColor: Colors.blue[400],
           centerTitle: true,
-          backgroundColor: Colors.blue[900],
         ),
-        body: Center(
+        body: SingleChildScrollView(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset('images/dice-${leftDiceIndex + 1}.png', width: 100),
-                  const SizedBox(width: 20),
-                  Image.asset('images/dice-${rightDiceIndex + 1}.png', width: 100),
-                ],
-              ),
-              Text("Sum = $sum", style: const TextStyle(fontSize: 30)),
-              const SizedBox(height: 20),
-              !published?
-              Padding(
-                padding: const EdgeInsets.all(20),
-                child: SizedBox(
-                  width: 200,
-                  height: 120, // Adjust height as needed
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      ElevatedButton(
-                        onPressed: published ? reset : roll,
-                        child: Text(
-                          published ? "Play Again" : "Roll",
-                          style: const TextStyle(
-                            fontSize: 20,
-                          ),
-                        ),
-                      ),
-                      ElevatedButton(
-                        onPressed: reset,
-                        child: const Text(
-                          "Reset",
-                          style: TextStyle(
-                            fontSize: 20,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ):
-              ElevatedButton(
-                onPressed: reset,
-                child: const Text(
-                  "Play Again",
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                ),
-              )
-              ,
-              SizedBox(height: 20),
-              if (verdict.isNotEmpty && verdict=="You Win!") 
-                Text(verdict, style: const TextStyle(fontSize: 30, color: Colors.green)),
-              if (verdict.isNotEmpty && verdict=="You Lose!") 
-                Text(verdict, style: const TextStyle(fontSize: 30, color: Colors.red)),
-              if (verdict.isEmpty)
-                const SizedBox(height: 30), // Add a SizedBox with height 30 when verdict is empty
+              imageSection(),
+              introSection(),
+              iconSection(),
+              paragraphSection(),
             ],
           ),
-        ),
+        ));
+  }
+
+  Widget paragraphSection() {
+    return const Padding(
+              padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+              child: Text(
+                  'bjugygy ig ugugug uigu ugu ug ugu ugu guu uug ufufufydytyrtfc guu uug ufufufydytyrtfc uuy yuftu guu uug ufufufydytyrtfc uuy yuftu guu uug ufufufydytyrtfc uuy yuftu guu uug ufufufydytyrtfc uuy yuftu uuy yuftuycyuc tt rxr ffy dtxr xrxhcyx r zrz gc td',
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black)),
+            );
+  }
+
+  Widget iconSection() {
+    return Container(
+      padding: EdgeInsets.all(14),
+      margin: EdgeInsets.all(14),
+      decoration: BoxDecoration(
+        color: Colors.blue[700],
+        borderRadius: BorderRadius.circular(16),
+
+      ),
+      child: const Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Column(
+            children: [
+              Icon(Icons.call, color: Colors.black, size: 30),
+              Text('Call', style: TextStyle(color: Colors.black, fontSize: 15)),
+            ],
+          ),
+          Column(
+            children: [
+              Icon(Icons.route, color: Colors.black, size: 30),
+              Text('Route', style: TextStyle(color: Colors.black, fontSize: 15)),
+            ],
+          ),
+          Column(
+            children: [
+              Icon(Icons.share, color: Colors.black, size: 30),
+              Text('Share', style: TextStyle(color: Colors.black, fontSize: 15)),
+            ],
+          ),
+        ],
       ),
     );
   }
 
-  void roll() {
-    setState(() {
-      leftDiceIndex = random.nextInt(6);
-      rightDiceIndex = random.nextInt(6);
-      sum = leftDiceIndex + rightDiceIndex + 2;
-      if (sum == 7 || sum == 11) {
-        verdict = "You Win!";
-        published=true;
-      } else if (sum == 2 || sum == 3 || sum == 12) {
-        verdict = "You Lose!";
-        published=true;
-      } else {
-        verdict = "";
-      }
-    });
+  Widget introSection() {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: [
+              const Text('Hello, This is a title',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              Text(
+                'This is a subtitle',
+                style: TextStyle(fontSize: 16, color: Colors.grey[600]),
+              ),
+            ],
+          ),
+          const Column(
+            children: [
+              Row(
+                children: [
+                  Icon(Icons.star, color: Colors.orange, size: 25),
+                  Text('41',
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold)),
+                ],
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 
-  void reset() {
-    setState(() {
-      leftDiceIndex = 0;
-      rightDiceIndex = 0;
-      sum = 0;
-      verdict = "";
-      isRolling = false;
-      published=false;
-    });
+  Widget imageSection() {
+    return Image.network(
+        "https://th.bing.com/th/id/R.fc99a71a82f12b952de000fcb6f9aa8c?rik=wQETi0Tsj4Aj2Q&riu=http%3a%2f%2fproject4gallery.com%2fwp-content%2fuploads%2f2016%2f03%2fbeautiful-nature-landscape-scenery-wide-5.jpg&ehk=9s5N2AWmjrEP%2fm7v%2fzbFZ%2b61XINT5u%2bVI8zoVd%2fRv%2bI%3d&risl=&pid=ImgRaw&r=0",
+        width: double.infinity,
+        height: 400,
+        fit: BoxFit.cover);
   }
 }
